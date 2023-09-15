1c284680d8b50319c8571b531fd02c5dd1994625
FarhanBasahir <farhanbasahir@gmail.com>
2023-09-15 08:10:50 +0800

    menambahkan file latihan2.php

---
+++ praktikum_web/latihan2.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kedua</title>
+</head>
+<body>
+  <?php
+  $nim = '1710110989';
+  $nama = 'Maria Mercedes';
+  $alamat = 'Jl. Gajah Mada No.4';
+  $nilai = '80';
+
+  echo "NIM : ", $nim, "<br>";
+  echo "Nama : ", $nama, "<br>";
+  echo "Alamat : ", $alamat, "<br>";
+  echo "Nilai : ", $nilai, "<br>";
+  ?>
+</body>
+</html>
