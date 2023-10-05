5f03ed7d6fb998d00fa811d551850f78959d4f84
FarhanBasahir <farhanbasahir@gmail.com>
2023-10-05 16:12:05 +0800

    Revisi 1

--- praktikum_web/modul7/tugasmodul7/tugas1.php
+++ praktikum_web/modul7/tugasmodul7/tugas1.php
@@ -1,36 +1,71 @@
-<?php 
-  //variabel 
-  $nim = $_POST['nim'];
-  $nama = $_POST['nama'];
-  $gender = $_POST['gender'];
-  $prodi = $_POST['prodi'];
-  $komentar = $_POST['komentar'];
-
-  //Biodata 
-  echo "Biodata Mahasiswa Politeknik Negeri Banjarmasin Jurusan Teknik Elektro <br>";
-
-  // Nama
-  echo "Nama : $nama <br>";
-  
-  // NIM
-  echo "NIM : $nim <br>";
-  
-  //Jenis Kelamin
-  echo "Jenis Kelamin : $gender <br>";
-
-  // Bahasa
-  echo "Bahasa : <br>";
-  if (isset($_POST['Indonesia'])){
-    echo "+ " . $_POST['Indonesia'] . "<br>";
-  }
-  if (isset($_POST['Inggris'])){
-    echo "+ " . $_POST['Inggris'] . "<br>";
-  }
-
-  // Program Studi
-  echo "Program Studi : $prodi <br>";
-
-  //Komentar
-  echo "Komentar : <br>";
-  echo $komentar;
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Selamat Datang di POLIBAN</title>
+</head>
+<body>
+  <h2 style="text-align: center">Selamat Datang Mahasiswa Politeknik Negeri Banjarmasin Jurusan Teknik Elektro</h2>
+  <table border="1" cellpadding="5" cellspacing="0" style="margin: auto">
+    <tr>
+      <td>Nama :</td>
+      <td>
+        <?php 
+          $nama = $_POST['nama'];
+          echo " $nama <br>";
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td>NIM :</td>
+      <td>
+        <?php 
+          $nim = $_POST['nim'];
+          echo " $nim <br>";
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Jenis Kelamin :</td>
+      <td>
+        <?php 
+          $gender = $_POST['gender'];
+          echo " $gender <br>";
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Bahasa :</td>
+      <td>
+        <?php 
+        if (isset($_POST['Indonesia'])){
+          echo "+ " . $_POST['Indonesia'] . "<br>";
+        }
+        if (isset($_POST['Inggris'])){
+          echo "+ " . $_POST['Inggris'] . "<br>";
+        }
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Program Studi :</td>
+      <td>
+        <?php 
+          $prodi = $_POST['prodi'];
+          echo " $prodi <br>";
+        ?>
+      </td>
+    </tr>
+    <tr>
+      <td>Komentar :</td>
+      <td>
+        <?php 
+          $komentar = $_POST['komentar'];
+          echo $komentar;
+        ?>
+      </td>
+    </tr>
+  </table>
+</body>
+</html>
