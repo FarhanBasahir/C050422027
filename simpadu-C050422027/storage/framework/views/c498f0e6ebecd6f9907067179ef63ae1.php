a3dceea4b37f6687ee61bfb509ff310557eb2d18
Farhan <137706234+FarhanBasahir@users.noreply.github.com>
2023-12-19 13:32:09 +0800

    Delete simpadu-C050422027 directory

--- simpadu-C050422027/storage/framework/views/c498f0e6ebecd6f9907067179ef63ae1.php
+++
@@ -1,4 +0,0 @@
-  <div class="simple-footer">
-      Copyright &copy; Stisla 2018
-  </div>
-<?php /**PATH C:\laragon\www\C050422027\simpadu-C050422027\resources\views/components/auth-footer.blade.php ENDPATH**/ ?>
